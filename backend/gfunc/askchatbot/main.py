eeb19fe637267bd5179b5894e758e4f649ca79f6
LauHingPui220242638 <113015644+LauHingPui220242638@users.noreply.github.com>
2023-09-18 03:47:39 +0000

    gfunc get

---
+++ backend/gfunc/askchatbot/main.py
@@ -0,0 +1,18 @@
+import functions_framework
+
+@functions_framework.http
+def hello_http(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    name = request_args.get('ask')
+    return 'Hello {}!'.format(name)
