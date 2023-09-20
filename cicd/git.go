72086fc8132aaee79ec6dbb0de394c1b6e30fd24
LauHingPui220242638 <113015644+LauHingPui220242638@users.noreply.github.com>
2023-09-20 16:03:45 +0000

    main

--- cicd/git.go
+++ cicd/git.go
@@ -7,7 +7,9 @@ import (
 func Git(args []string)  {
 	err := CheckNum(args, 3, `
 	please enter:
-		cicd git <command>
+			cicd git push <message>
+			cicd git m2b <branch>
+			cicd git b2m <branch>
 			`)
 	if err != nil {
 		fmt.Println(err.Error())
