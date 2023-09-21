c822df3b245b49fa7c4008b76c196a0e277fc957
LauHingPui220242638 <113015644+LauHingPui220242638@users.noreply.github.com>
2023-09-21 09:49:42 +0000

    tf-init

---
+++ iac/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("../secretes/fyp-open-data-hackathon-8644213ecdba.json")
+
+  project = "fyp-open-data-hackathon"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
