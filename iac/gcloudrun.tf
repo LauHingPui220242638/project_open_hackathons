dcf52abd5b096005d54779411f64dd082d89a81b
LauHingPui220242638 <113015644+LauHingPui220242638@users.noreply.github.com>
2023-09-23 15:31:43 +0000

    all17

--- iac/gcloudrun.tf
+++ iac/gcloudrun.tf
@@ -19,20 +19,46 @@
 
 resource "google_artifact_registry_repository" "artifact-registry-cloudrun" {
   repository_id = "cloudrun"
-  location      = "asia-east1"
+  location      = var.region
   format        = "docker"
 }
 
 
-resource "google_cloud_run_service" "cloud-run-backend" {
-  name = "cloudrun-backend"
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-backend"
   location = var.region
 
-  template {
-	spec {
-  	containers {
-        image = "asia-east1-docker.pkg.dev/fyp-open-data-hackathon/backend/backend:latest"
+  
+  
+    template {
+    spec {
+   
+      containers {
+      image = "asia-east1-docker.pkg.dev/fyp-open-data-hackathon/backend/backend:latest"
+      }
+    }
+    metadata {
+    annotations = {
+      "autoscaling.knative.dev/maxScale"      = "1"
       }
     }
   }
+  
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
 }
