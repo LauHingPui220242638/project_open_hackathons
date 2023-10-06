2e43529fa2691e6c226e0198cb26050a3132e9bd
LauHingPui220242638 <113015644+LauHingPui220242638@users.noreply.github.com>
2023-10-06 10:20:41 +0000

    fixcbuild

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,4 @@
+cd /workspaces/
 curl https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.13.5-stable.tar.xz -O
 tar xf flutter_linux_3.13.5-stable.tar.xz
 rm flutter_linux_3.13.5-stable.tar.xz
