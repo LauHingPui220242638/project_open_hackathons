85adbf9c055e57bf8dd68f962b3472b73881458e
EvanManHoSing <113015451+EvanManHoSing@users.noreply.github.com>
2023-10-16 19:58:49 +0000

    modified:   vertexai/langchain-bigquery.py

--- vertexai/langchain-bigquery.py
+++ vertexai/langchain-bigquery.py
@@ -1,13 +1,14 @@
 from langchain.document_loaders import BigQueryLoader
 
+
 def bigquery(query):
     loader = BigQueryLoader(query)
     data = loader.load()
     return data
 
 BASE_QUERY = """
-SELECT * FROM `fyp-open-data-hackathon.test.test` LIMIT 10
-"""
+SELECT * FROM `fyp-open-data-hackathon.test.test` LIMIT 10;"""
+
 
 result = bigquery(BASE_QUERY)
 print(result)
