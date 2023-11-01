7a1f34fd2a9be2a36e9050089fcd1201b1e50e00
LauHingPui220242638 <113015644+LauHingPui220242638@users.noreply.github.com>
2023-11-01 15:36:51 +0000

    fixall2

--- backend/callbackend.sh
+++ backend/callbackend.sh
@@ -1,6 +1,6 @@
-# curl https://cloudrun-backend-qskzidtbhq-de.a.run.app/ask?api_key=AIzaSyA5jhZl3dPpOsr5DX4_pCgA4ibI7sDfNOM \
+backend=https://cloudrun-backend-qskzidtbhq-de.a.run.app
 
-backend=http://localhost:8080
+# backend=http://localhost:8080
 api_key=AIzaSyDaxF5on_M6SClhdMRn64TQsrvHUC3RfAc
 curl -X 'POST' \
   "${backend}/ask?api_key=${api_key}" \
