c17313490810158d9d818f753498babd5408a25b
LauHingPui220242638 <113015644+LauHingPui220242638@users.noreply.github.com>
2023-11-01 16:21:55 +0000

    fixall7

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,9 +2,9 @@ sudo apt update
 sudo apt upgrade
 
 cd /workspaces/
-curl https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.13.5-stable.tar.xz -O
-tar xf flutter_linux_3.13.5-stable.tar.xz
-rm flutter_linux_3.13.5-stable.tar.xz
+curl https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.13.9-stable.tar.xz -O
+tar xf flutter_linux_3.13.9-stable.tar.xz
+rm flutter_linux_3.13.9-stable.tar.xz
 
 
 apt-get install -y gnupg software-properties-common
